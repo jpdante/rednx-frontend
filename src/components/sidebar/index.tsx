0e9e33041100cfc066b04e1fb54793fa7c4966ed
João Pedro Caires <joaopdante@gmail.com>
2020-06-04 16:36:48 -0300

    Convert sidebar to hook

--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -1,30 +1,19 @@
 import React from "react";
-import { Link } from "@reach/router";
-import { withTranslation, WithTranslation } from "react-i18next";
-
-import styles from "./sidebar.module.scss";
 
 import MainNav from "./main-nav";
-import FollowingNav from './following-nav';
+import FollowingNav from "./following-nav";
 import CategoryNav from "./category-nav";
 
-class SideBar extends React.Component<WithTranslation> {
-  state = {
-    isAuthenticated: false,
-  };
-
-  logout = async () => {};
-
-  render() {
-    const { t } = this.props;
-    return <div className="sidebar">
+function SideBar() {
+  return (
+    <div className="sidebar">
       <MainNav />
       <hr />
       <FollowingNav />
       <hr />
       <CategoryNav />
-    </div>;
-  }
+    </div>
+  );
 }
 
-export default withTranslation()(SideBar);
+export default SideBar;
