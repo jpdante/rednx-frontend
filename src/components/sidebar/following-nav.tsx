969e96ade0db586ab387af49b88a6022cc09c6c7
João Pedro Caires <joaopdante@gmail.com>
2020-06-04 16:37:42 -0300

    Remove unused vars

--- src/components/sidebar/following-nav.tsx
+++ src/components/sidebar/following-nav.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 import NavLink from "../sidebar/nav-link";
 
